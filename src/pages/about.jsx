b6fc64816299dc8b47bb172718dd5ba5edd0faa7
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2024-10-18 11:51:53 +0100

    added about and contact info to relevant pages

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,6 +6,7 @@ const About = () => {
     return (
         <div>
             <h1>ABOUT</h1>
+            <p>I decided to create this quiz in order to solidify my html, css and javascript knowledge. I proceeded to convert this quiz into a REACT app to solidify and extend my REACT knowledge and skills.</p>
         </div>
     )
 }
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
     return (
         <div>
             <h1>CONTACT US</h1>
-
+            <p>swainejohn477@gmail.com</p>
         </div>
     )
 }
